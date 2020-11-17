4c10ac688355cc60d8917a4e76eadc97bb705fdd
BVenturelli <64107807+BVenturelli@users.noreply.github.com>
2020-11-16 23:54:02 -0300

    Lista de elementos desde Js

--- js/mascotas.js
+++ js/mascotas.js
@@ -1,1 +1,29 @@
-console.log('mascotas');
+// console.log('mascotas');
+const listaMascotas = document.getElementById("listamascotas ");
+
+let mascotas = [{
+    tipo: "Gato",
+    nombre: "Manchas",
+    propietario: "Esteban"
+
+}];
+
+function listarMascotas() {
+    const htmlMascotas = mascotas.map((mascota, indice) => `
+                <tr>
+                    <th scope="row ">${indice}</th>
+                    <td>${mascota.tipo}</td>
+                    <td>${mascota.nombre}</td>
+                    <td>${mascota.propietario}</td>
+                    <td>
+                        <div class="btn-group " role="group " aria-label="Basic example ">
+                            <button type="button " class="btn btn-info "><i class="far fa-edit "></i></button>
+                            <button type="button " class="btn btn-danger "><i class="fas fa-trash-alt "></i></button>
+                        </div>
+                    </td>
+                </tr>
+    `).join("");
+
+    listaMascotas.innerHTML = htmlMascotas;
+}
+listarMascotas();
